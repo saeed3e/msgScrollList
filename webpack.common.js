674adb61300a0c6e06c5fcad723759a09870c37f
Mohd Saeed <mohd.saeed@oracle.com>
2022-08-22 23:58:07 +0530

    fixed vulnerabilities

--- webpack.common.js
+++ webpack.common.js
@@ -45,38 +45,8 @@ module.exports = {
                 ],
             },
             {
-                test: /\.(png|svg|jpg|jpeg|gif)$/,
-                use: [{
-                    loader: 'file-loader',
-
-                    options: {
-                        name: '[name].[ext]',
-                        outputPath: 'images/',
-                        loader: 'image-webpack-loader',
-                        options: {
-                            mozjpeg: {
-                                progressive: true,
-                                quality: 65
-                            },
-                            // optipng.enabled: false will disable optipng
-                            optipng: {
-                                enabled: true,
-                                optimizationLevel: 7
-                            },
-                            pngquant: {
-                                quality: '65-90',
-                                speed: 4
-                            },
-                            gifsicle: {
-                                interlaced: false,
-                            },
-                            // the webp option will enable WEBP
-                            webp: {
-                                quality: 75
-                            }
-                        }
-                    },
-                }]
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
             },
         ]
     },
